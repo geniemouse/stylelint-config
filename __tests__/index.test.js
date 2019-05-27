6607d993d2e7870776518d580c9156d509190611
Lucy Barker <lucy@geniemouse.com>
2019-05-27 18:38:43 +0100

    Update tests: check plugins present

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,8 +2,12 @@ const stylelintConfig = require("../");
 
 describe("@geniemouse/stylelint-config:", () => {
     test("returns an object with plugins and rules properties", () => {
-        expect(stylelintConfig).toHaveProperty("plugins");
-        expect(stylelintConfig).toHaveProperty("rules");
+        expect(stylelintConfig).toHaveProperty("plugins", expect.any(Array));
+        expect(stylelintConfig).toHaveProperty("rules", expect.any(Object));
+    });
+
+    test("ensure stylelint-a11y plugin is present", () => {
+        expect(stylelintConfig.plugins).toContain("stylelint-a11y");
     });
 
     test("matches the expected output", () => {
